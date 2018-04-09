67ec5c0bafd27cabd57c6c81ba58499d5d1cc2fd
Tobias Funke <thedavetheory@yahoo.com>
2018-04-08 20:12:05 -0400

    Update emailScraper.py

--- emailScraper.py
+++ emailScraper.py
@@ -6,6 +6,7 @@
 # scrape.
 #
 # Revision  1.0     -   02/12/2018- Initial creation of script
+# Revision  1.1     -   04/06/2018- Added in some more error handling
 
 import re
 from collections import deque
@@ -35,47 +36,52 @@ emails = set()
 # process urls one by one until we exhaust the queue
 while len(new_urls):
 
-    # move next url from the queue to the set of processed urls
-    url = new_urls.popleft()
-    processed_urls.add(url)
-
-    # extract base url to resolve relative links
-    parts = urlsplit(url)
-    base_url = "{0.scheme}://{0.netloc}".format(parts)
-    path = url[:url.rfind('/') + 1] if '/' in parts.path else url
-
-    # get url's content
-    print("Processing %s" % url)
     try:
-        response = requests.get(url)
-    except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-        # ignore pages with errors
-        continue
+        # move next url from the queue to the set of processed urls
+        url = new_urls.popleft()
+        processed_urls.add(url)
 
-    # extract all email addresses and add them into the resulting set
-    new_emails = set(re.findall(r"[a-z0-9.\-+_]+@[a-z0-9.\-+_]+\.[a-z]+", response.text, re.I))
-    emails.update(new_emails)
+        # extract base url to resolve relative links
+        parts = urlsplit(url)
+        base_url = "{0.scheme}://{0.netloc}".format(parts)
+        path = url[:url.rfind('/') + 1] if '/' in parts.path else url
 
-    try:
-        # create a beautiful soup for the html document
-        soup = BeautifulSoup(response.text)
+        # get url's content
+        print("Processing %s" % url)
+        try:
+            response = requests.get(url)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+            # ignore pages with errors
+            continue
 
-        # find and process all the anchors in the document
-        for anchor in soup.find_all("a"):
-            # extract link url from the anchor
-            link = anchor.attrs["href"] if "href" in anchor.attrs else ''
-            # resolve relative links
-            if link.startswith('/'):
-                link = base_url + link
-            elif not link.startswith('http'):
-                link = path + link
-            # add the new url to the queue if it was not enqueued nor processed yet
-            if not (link in new_urls or link in processed_urls):
-                # only add urls that are sub urls of the starting url
-                if starting_url in link:
-                    new_urls.append(link)
+        # extract all email addresses and add them into the resulting set
+        new_emails = set(re.findall(r"[a-z0-9.\-+_]+@[a-z0-9.\-+_]+\.[a-z]+", response.text, re.I))
+        emails.update(new_emails)
+
+        try:
+            # create a beautiful soup for the html document
+            soup = BeautifulSoup(response.text)
+
+            # find and process all the anchors in the document
+            for anchor in soup.find_all("a"):
+                # extract link url from the anchor
+                link = anchor.attrs["href"] if "href" in anchor.attrs else ''
+                # resolve relative links
+                if link.startswith('/'):
+                    link = base_url + link
+                elif not link.startswith('http'):
+                    link = path + link
+                # add the new url to the queue if it was not enqueued nor processed yet
+                if not (link in new_urls or link in processed_urls):
+                    # only add urls that are sub urls of the starting url
+                    if starting_url in link:
+                        new_urls.append(link)
+        except Exception:
+            # if the URL is too long this can error out
+            continue
+
     except Exception:
-        # if the URL is too long this can error out
+        # if some error occurs
         continue
 
 # remove duplicates from emails
@@ -94,3 +100,4 @@ f = open(outfile, 'w')
 for email in final_emails:
     f.write(email + '\n')
 f.close()
+

140cf3aeea100577754169ecdb1cddf9f2b327f8
Tobias Funke <thedavetheory@yahoo.com>
2019-01-16 15:53:20 -0500

    Update emailScraper.py

--- emailScraper.py
+++ emailScraper.py
@@ -1,4 +1,7 @@
-#! Python3
+#! python3
+# emailScraper.py -v 1.3
+# Author- David Sullivan
+#
 # Credit to Michael Shilov from scraping.pro/simple-email-crawler-python for the majority of this code
 #
 # As a reminder, web scraping for the purpose of SPAM or hacking is illegal. This tool has been provided for
@@ -9,10 +12,16 @@
 # Revision  1.1     -   04/06/2018- Added in some more error handling
 # Revision  1.2     -   04/18/2018- Added in ability to blacklist words in links (to avoid looping), and the ability to
 #                                   process sub-domains
+# Revision  1.3     -   08/07/2018- Added in functionality to write to output as it runs, in case the script breaks
+# Revision  1.4     -   01/16/2019- Added a timeout for stale requests, supression for error messages related to SSL
 
 import re
 from collections import deque
 from urllib.parse import urlsplit
+
+# disable insecure request warning
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 import requests.exceptions
 from bs4 import BeautifulSoup
@@ -23,7 +32,7 @@ starting_url = str(input(r'Put in the full URL of where you want to start crawli
 # set domain name
 domain_name = str(input(r'If you want to include different subdomains, enter the domain name here(ex: To include '
                         r'"my.test.local" as well as "test.local", enter "test.local") If you want to stick to the '
-                        r'subdomain of the starting URL, leave this blank'))
+                        r'subdomain of the starting URL, leave this blank: '))
 
 domain_name = starting_url if domain_name == '' else domain_name
 
@@ -39,11 +48,11 @@ new_urls = deque([starting_url])
 # a set of urls that we have already crawled
 processed_urls = set()
 
-# a set of crawled emails
-emails = set()
+# create an empty list for the emails variable
+emails = list()
 
 # don't include links with the following (to avoid loops)
-bad_link_words = ['##']
+bad_link_words = ['##', '.pdf', '.mp3', '.mp4', '.mpg', '.wav', '.jpg', '.png', '.gif']
 
 
 # process to handle checking the bad_link_words list
@@ -68,19 +77,24 @@ while len(new_urls):
         # get url's content
         print("Processing %s" % url)
         try:
-            response = requests.get(url)
+            response = requests.get(url, verify=False, timeout=10)
         except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
             # ignore pages with errors
             continue
 
-        # extract all email addresses and add them into the resulting set
+        # extract all email addresses
         new_emails = set(re.findall(r"[a-z0-9.\-+_]+@[a-z0-9.\-+_]+\.[a-z]+", response.text, re.I))
-        emails.update(new_emails)
+
+        # write them to output file
+        f = open(outfile, 'a+')
+        for email in new_emails:
+            f.write(email + '\n')
+        f.close()
 
         # noinspection PyBroadException
         try:
             # create a beautiful soup for the html document
-            soup = BeautifulSoup(response.text)
+            soup = BeautifulSoup(response.text, "html.parser")
 
             # find and process all the anchors in the document
             for anchor in soup.find_all("a"):
@@ -105,6 +119,13 @@ while len(new_urls):
     except Exception:
         # if some error occurs
         continue
+
+# open the output file and import all the crawled emails
+f = open(outfile, 'r')
+for email in f:
+    email = email.replace('\n', '')
+    emails.append(email)
+f.close()
 
 # remove duplicates from emails
 emails = set([email.lower() for email in emails])
